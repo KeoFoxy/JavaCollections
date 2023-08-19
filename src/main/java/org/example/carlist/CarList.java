90d33392b8274299bc384c2c4b5fd8f9203a1dd5
KeoFoxy <arasdiel68@gmail.com>
2023-08-19 21:50:39 +0300

    CarList methods list

--- src/main/java/org/example/carlist/CarList.java
+++ src/main/java/org/example/carlist/CarList.java
@@ -1,2 +1,15 @@
-package org.example.carlist;public interface CarList {
+package org.example.carlist;
+
+import org.example.entity.Car;
+
+public interface CarList {
+    Car get(int index);
+
+    int size();
+
+    void clear();
+    void add(Car car);
+
+    boolean removeAt(int index);
+    boolean remove(Car car);
 }
