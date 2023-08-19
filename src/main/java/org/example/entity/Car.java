6fa7622fad28bdac4ce49daad71cdbadfd4c28e5
KeoFoxy <arasdiel68@gmail.com>
2023-08-19 21:50:18 +0300

    Car entity

--- src/main/java/org/example/entity/Car.java
+++ src/main/java/org/example/entity/Car.java
@@ -1,2 +1,19 @@
-package org.example.entity;public class Car {
+package org.example.entity;
+
+public class Car {
+    private int number;
+    private String brand;
+
+    public Car(int number, String brand) {
+        this.number = number;
+        this.brand = brand;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
 }
