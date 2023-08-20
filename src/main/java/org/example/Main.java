6ddf8a2fea4545b17216352be3b858ab039f87b9
KeoFoxy <arasdiel68@gmail.com>
2023-08-20 21:59:08 +0300

    Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,22 @@
 package org.example;
 
+import org.example.cararraylist.CarArrayList;
+import org.example.carlist.CarList;
+import org.example.entity.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        CarList cars = new CarArrayList();
+        for(int i = 0; i < 10; i++) {
+            cars.add(new Car(i, "Porsche " + i));
+        }
+
+        for(Car car: cars) {
+            System.out.println(car.getNumber() + " " + car.getBrand());
+        }
     }
 }
